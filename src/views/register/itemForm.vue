f1e3c37825569a1d9cb170abb50f1b0edead982f
Raul <raulramosneves9@gmail.com>
2026-04-04 09:38:11 -0300

    em itemForm foi adicionado cadastrar item, nome do item, preço, categorias e input para ver se os items estão disponiveis

---
+++ src/views/register/itemForm.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Cadastrar Item</h1>
+    <span class="inputName">Nome do Item</span>
+    <input type="text" placeholder="Ex: X-Burguer">
+    <span class="inputName">Preço (R$)</span>
+    <input type="text" placeholder="0,00">    
+    <span class="inputName">Categoria</span>
+    <select name="category">
+        <option value="lanche">Lanche</option>
+        <option value="bebida">Bebida</option>
+        <option value="sobremesa">Sobremesa</option>
+    </select>
+    <span class="inputName">Disponível</span>
+    <input type="checkbox">
+</template>
+
+<style scoped>
+</style>
